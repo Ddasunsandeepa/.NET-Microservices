c556ec63171476bf90e90876bd972eced41096e1
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-09-17 11:58:51 +0530

    updated seeding and migration

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,10 +1,13 @@
+using PlatformService.Models;
+
 namespace PlatformService.Data
 {
   public static class PrepDb
   {
     public static void PrepPopulation(IApplicationBuilder app, bool isProd)
     {
-      using (var serviceScope = app.ApplicationServices.CreateScope()){
+      using (var serviceScope = app.ApplicationServices.CreateScope())
+      {
         var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
         if (context != null)
         {
@@ -19,7 +22,20 @@ namespace PlatformService.Data
 
     private static void SeedData(AppDbContext context, bool isProd)
     {
+      if (!context.Platforms.Any())
+      {
+        Console.WriteLine("Seeding Data...");
 
+        context.Platforms.AddRange(
+          new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+          new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+          new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+        );
+      }
+      else
+      {
+        Console.WriteLine("We already have data");
+      }
     }
   }
 }
