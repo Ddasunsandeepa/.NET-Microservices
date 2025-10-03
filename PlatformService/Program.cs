1876eafd13e3d40b0925cf48f0066b2bc140bae7
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-10-03 11:18:57 +0530

    program file updated

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,46 +1,79 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.AsyncDataServices;
+using PlatformService.SyncDataServices.Http;
+using PlatformService.SyncDataServices.Grpc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Force Kestrel to listen on port 80
+// Force Kestrel to listen on port 80 (container friendly)
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(80);
 });
 
-// Register DbContext
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+// Environment
+var env = builder.Environment;
 
-// Register Repository (Interface + Implementation)
+// Configure DbContext
+if (env.IsProduction())
+{
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMem Db");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseInMemoryDatabase("InMem"));
+}
+
+// Register repository
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+// Register external services
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+builder.Services.AddGrpc();
 
 // Register AutoMapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-// Add services to the container.
+// Add controllers
 builder.Services.AddControllers();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// OpenAPI/Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Console.WriteLine($"--> CommandService Endpoint {builder.Configuration["CommandService"]}");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware & Pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // optional
+
+app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcPlatformService>();
 
-// Call PrepDb here at startup
+// Expose proto file
+app.MapGet("/protos/platforms.proto", async context =>
+{
+    await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+});
+
+// Seed database
 PrepDb.PrepPopulation(app, app.Environment.IsProduction());
+
 Console.WriteLine($"Environment: {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}");
 
-
 app.Run();
-
